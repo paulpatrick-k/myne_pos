99b65e1e7b80ff3f3116d017d0821bea2ee1e071
paulpatrick-k <paulpatrickkariuki@gmail.com>
2026-05-21 09:08:31 +0400

    Add .env to assets, fix USB host, disable shrinking

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,3 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
 import java.util.Properties
 import java.io.FileInputStream
 
@@ -14,10 +7,17 @@ if (keystorePropertiesFile.exists()) {
     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
 android {
     namespace = "com.mynepos.retail"
-    compileSdk = 34
-    ndkVersion = "27.0.12077973"
+    compileSdk = 36
+
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -25,21 +25,15 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
         applicationId = "com.mynepos.retail"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
-
-        ndk {
-            abiFilters += setOf("arm64-v8a", "armeabi-v7a")
-        }
     }
 
     signingConfigs {
@@ -51,20 +45,12 @@ android {
         }
     }
 
-    lint {
-        checkReleaseBuilds = false
-        abortOnError = false
-    }
-
     buildTypes {
         getByName("release") {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             isShrinkResources = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            // No proguardFiles
         }
     }
 }
