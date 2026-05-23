e827cdf3253c06e82c5cef9e7251816207c3a629
paulpatrick-k <paulpatrickkariuki@gmail.com>
2026-05-23 16:59:28 +0400

    Fix: Use computer's IP for mobile connectivity

--- lib/features/subscription/paywall_screen.dart
+++ lib/features/subscription/paywall_screen.dart
@@ -33,7 +33,7 @@ class _PaywallScreenState extends State<PaywallScreen> {
     try {
       final httpClient = http.Client();
       final response = await httpClient.post(
-        Uri.parse('http://127.0.0.1:3000/init-payment'),
+        Uri.parse('http://192.168.1.66:3000/init-payment'),
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode({
           'amount': 105000,
